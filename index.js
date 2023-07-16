b6beb526509ad055a73dec2f12a4d0cf8fe3133e
chethanb8055 <101992441+chethanb8055@users.noreply.github.com>
2023-07-16 10:54:11 +0530

    3rd commit

--- index.js
+++ index.js
@@ -99,10 +99,10 @@ function renderWeatherInfo(weatherInfo){
    countryIcon.src =`https://flagcdn.com/144x108/${weatherInfo?.sys?.country.toLowerCase()}.png`;
    desc.innerText = weatherInfo?.weather?.[0]?.description;
    weatherIcon.src = `http://openweathermap.org/img/w/${weatherInfo?.weather?.[0]?.icon}.png`;
-   temp.innerText =weatherInfo?.main?.temp;
-   windspeed.innerText = weatherInfo?.wind?.speed;
-   humidity.innerText =weatherInfo?.main?.humidity;
-   cloudness.innerText =weatherInfo?.clouds?.all;
+   temp.innerText =`${weatherInfo?.main?.temp}°C`;
+   windspeed.innerText =`${weatherInfo?.wind?.speed}m/s`;
+   humidity.innerText =`${weatherInfo?.main?.humidity}%`;
+   cloudness.innerText =`${weatherInfo?.clouds?.all}%`;
 
 }
 
@@ -125,11 +125,8 @@ function showPosition(position){
          lon : position.coords.longitude
 
     }
-  
-  
     sessionStorage.setItem("user-coordinates",JSON.stringify(userCoordinates))
-    fetchUserWeatherInfo(userCoordinates)
-    
+    fetchUserWeatherInfo(userCoordinates)   
 }
 
 
